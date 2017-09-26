b1e8db9047aba5f9401b366474cb451ff85bbc88
Ethan Petersen <ethanpetersen@Ethans-MacBook-Pro.local>
2017-09-26 11:54:38 -0400

    Added network to first default banks below ratio requirement and then default banks whose capital is lower than their exposure to those that previously failed.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Avalanche
 Implementations of contagion models in financial networks.
+
+# Development
+We develop in Jupyter notebooks using the [BeakerX enhancements](http://beakerx.com/documentation).
--- contagion.py
+++ contagion.py
@@ -18,7 +18,8 @@ def binarize_probabilities(mat):
     num_probs = mat.shape[0] * mat.shape[1]
 
     # Another probability matrix is generated and to determine 1 or 0 we...
-    probs = np.random.negative_binomial(1, .7, size=num_probs).reshape(mat.shape)
+    # probs = np.random.negative_binomial(1, .7, size=num_probs).reshape(mat.shape)
+    probs = np.random.uniform(size=num_probs).reshape(mat.shape)
 
     # ... compare the generated probability against the given probability matrix
     # if it is less than, then the entry is a 1 otherwise it is a 0
@@ -152,7 +153,7 @@ class DeterministicRatioNetwork:
         num_defaults = 0
         while True:  # Cascade until no more defaults
             for i in range(self.size):
-                # """
+                """
                 capital = self.liabilities[i, i]
                 assets = self.liabilities[i, :].sum()
                 liabilities = self.liabilities[:, i].sum()
@@ -161,22 +162,106 @@ class DeterministicRatioNetwork:
                     self.default(i)
                     self.recover(i)
                     num_defaults += 1
-                # """
-                """Ratio cascade
+                """
+                #"""Ratio cascade
                 capital = self.liabilities[i, i]
                 assets = self.liabilities[i, :].sum() - capital
                 liabilities = self.liabilities[:, i].sum() - capital
                 
                 if liabilities != 0 and capital != 0:
                     ratios[i] = capital / liabilities
-                    if capital / liabilities < 0.08:
+                    if capital / liabilities < 0.1:
                         self.default(i)
                         self.recover(i)
                         num_defaults += 1
-                """
+                #"""
             if previous_defaults == num_defaults:
                 break
             previous_defaults = num_defaults
+        return ratios, num_defaults
+
+    def show(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        ax.set_aspect('equal')
+        plt.imshow(self.liabilities, interpolation='nearest', cmap=plt.cm.hot)
+        plt.colorbar()
+        plt.show()
+
+        
+class TestNetwork:
+    
+    def __init__(self, size, liabilities=None, recovery_rate=0.0, initial_cap=10000):
+        self.size = size
+        self.liabilities = liabilities if liabilities is not None else np.zeros((size, size))
+        self.recovery_rate = recovery_rate
+        self.initial_cap = initial_cap
+
+    def reset_net(self):
+        for i in range(self.size):
+            for j in range(i + 1, self.size):
+                self.liabilities[i, j] = max(self.liabilities[i, j] - self.liabilities[j, i], 0)
+                self.liabilities[j, i] = max(self.liabilities[j, i] - self.liabilities[i, j], 0)
+
+    def default(self, i):
+        self.liabilities[i, i] = 0
+
+    def recover(self, i):
+        for j in range(self.size):
+            if i != j:
+                self.liabilities[j, j] += self.recovery_rate * self.liabilities[j, i]
+                self.liabilities[j, i] = 0
+
+    def step(self):
+        # Select entry to add debt to
+        rand_i = np.random.randint(self.size)
+        rand_j = np.random.randint(self.size)
+        rand_prop = 0.1
+
+        # Add debt
+        capital = self.liabilities[rand_i, rand_i]
+        assets = self.liabilities[rand_i, :].sum() - capital
+        liabilities = self.liabilities[:, rand_i].sum() - capital
+
+        if capital != 0:
+            self.liabilities[rand_i, rand_j] += rand_prop * self.liabilities[rand_i, rand_i]
+            self.liabilities[rand_i, rand_i] -= rand_prop * self.liabilities[rand_i, rand_i]
+        else:
+            if rand_i == rand_j:
+                self.liabilities[rand_i, rand_j] = self.initial_cap
+
+        # Settle
+        ratios = np.zeros(self.size)
+        previous_defaults = 0
+        num_defaults = 0
+        defaulted_banks = []
+        for i in range(self.size):
+            capital = self.liabilities[i, i]
+            assets = self.liabilities[i, :].sum() - capital
+            liabilities = self.liabilities[:, i].sum() - capital
+
+            if liabilities != 0 and capital != 0:
+                ratios[i] = capital / liabilities
+                if capital / liabilities < 0.1:
+                    defaulted_banks.append(i)
+                    num_defaults += 1
+        while True:  # Cascade until no more defaults
+            for i in range(self.size):
+                if i in defaulted_banks: continue
+                capital = self.liabilities[i, i]
+                
+                for defaulted_bank in defaulted_banks:
+                    exposure = self.liabilities[i, defaulted_bank]
+                    if capital < exposure:
+                        defaulted_banks.append(i)
+                        num_defaults += 1
+                        break
+            if previous_defaults == num_defaults:
+                break
+            previous_defaults = num_defaults
+        for default in defaulted_banks:
+            self.liabilities[:, default] = 0
+            self.liabilities[default, :] = 0
         return ratios, num_defaults
 
     def show(self):
