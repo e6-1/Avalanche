c9d8b1128ab978bf63813eb28a49c7440b3341f2
Ethan Petersen <ethanopetersen@gmail.com>
2018-02-28 15:16:47 +0100

    Refactored json to csv to ignore 0-index value.

--- json_to_csv.py
+++ json_to_csv.py
@@ -6,13 +6,15 @@ import pandas as pd
 import numpy as np
 import sys
 
-path_to_json = sys.argv[1]
+# path_to_json = sys.argv[1]
+path_to_json = 'NormalDistResults/'
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 
 for i, json_file in enumerate(json_files):
     with open(path_to_json + json_file) as f:
         data = json.load(f)
-        arr = np.zeros((100, 1))
+        arr = np.zeros((101, 1))
         for k in data:
             arr[int(k)] = data[k]
+        arr[:100] = arr[1:]
         np.savetxt(path_to_json + "result{0}.csv".format(i), arr)
